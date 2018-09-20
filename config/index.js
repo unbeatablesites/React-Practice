1d4a6425ba72cecaa8535aec4dcb3ba3d6cce4d7
Fuzoka <unbeatablesites09@gmail.com>
2018-09-20 17:49:57 -0400

    Practice makes perfect

--- config/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-   
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newscraper";
-
-exports.port = process.env.PORT || 3000
-exports.origin = process.env.ORIGIN || `http://localhost:${exports.port}`
-exports.init = function() {return require('./config.json')}
