bcaedab0238e689a22c4b8de6207d311ffea88d3
Fuzoka <unbeatablesites09@gmail.com>
2018-09-29 14:14:24 -0400

    More react practice

--- 07-Ins_Mern/create-react-express/server.js
+++ 07-Ins_Mern/create-react-express/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
--- 11-Stu_ReactRouter/Unsolved/server.js
+++ 11-Stu_ReactRouter/Unsolved/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
