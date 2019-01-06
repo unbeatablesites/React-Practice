ba7ed4f9579155bb92bdac808412e908a9bfc168
Fuzoka <unbeatablesites09@gmail.com>
2019-01-05 23:08:09 -0500

    Generating react project

---
+++ Generating react project/jsx/src/index.js
@@ -0,0 +1,6 @@
+//Import react and react dom Libraries
+    impot React from 'react';
+    improt ReactDom from 'react-dom';
+
+//Create a react component
+//Take the ract component and show it on the screen
